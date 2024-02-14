668901715f95aa8583042f6278cdf1c73a41bd7a
Andres <andres.costa@gmail.com>
2024-02-14 13:28:42 -0500

    fixed issues with directories.

--- internal/queue/provider/filequeue.go
+++ internal/queue/provider/filequeue.go
@@ -81,7 +81,7 @@ func (f *FileQueue[T]) writeData(data T) error {
 
 	f.mutex.Lock()
 	defer f.mutex.Unlock()
-	if _, err := ioutil.WriteToRandomFile(f.directory, preffix, suffix, buffer.Bytes()); err != nil {
+	if _, err := ioutil.WriteToRandomFile(f.directory, preffix, suffix, buffer.Bytes(), 0o700); err != nil {
 		return err
 	}
 	return nil
