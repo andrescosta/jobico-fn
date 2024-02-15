2e5f6dbc3fe0b42a72afdbc3c0b8cc615f4a787c
Andres <andres.costa@gmail.com>
2024-02-15 12:58:21 -0500

    fiexed scripts errors

--- scripts/startall.sh
+++ scripts/startall.sh
@@ -14,7 +14,6 @@ jobico::startall(){
     local files=( $(jobico::services) )
     for t in "${files[@]}"; do
        $ROOT/bin/${t} --env:basedir=$ROOT/bin --env:workdir=$ROOT/work &
-       sleep 1
     done
 }
 
--- scripts/stopall.sh
+++ scripts/stopall.sh
@@ -1,7 +1,7 @@
 jobico::killall(){
     local pids=( $(ps f | grep ../bin/ | awk '{print $1}') )
     for t in "${pids[@]}"; do
-       kill ${t}
+       kill ${t} 2>>/dev/null
     done
 }
 
